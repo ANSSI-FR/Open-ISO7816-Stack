ccf192bf1f80fd1c28a97f0b5b769b3709dfc7a2
boris <boris.simunovic@etu.esisar.grenoble-inp.fr>
2017-12-29 11:53:52 +0100

    Suppression du pooling bouton bleu dans main()

--- src/main.c
+++ src/main.c
@@ -8,14 +8,7 @@
 int main(void){	
 	HAL_Init();
 	
-	while(1){
-		if(HAL_GPIO_ReadPin(GPIOA, PIN_BOUTON_BLEU) == GPIO_PIN_SET){
-			HAL_GPIO_WritePin(GPIOD, PIN_LED_VERTE, GPIO_PIN_SET);
-		}
-		else{
-			HAL_GPIO_WritePin(GPIOD, PIN_LED_VERTE, GPIO_PIN_RESET);
-		}
-	}
+	while(1);
 	
 	return 0;
 }
