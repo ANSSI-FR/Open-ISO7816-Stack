dade559abef5a42e991cb81d4c6f1f2a7f57ecbc
boris <boris.simunovic@etu.esisar.grenoble-inp.fr>
2018-02-21 17:07:36 +0100

    Amelioration de la fonction READER_HAL_Init() Notamment les valeurs de retour

--- src/reader_hal.c
+++ src/reader_hal.c
@@ -13,10 +13,10 @@ READER_Status READER_HAL_Init(void){
 
 
 	/* Initialisation de tous les peripheriques materiels */
-	READER_PERIPH_Init();
+	if(READER_PERIPH_Init() != READER_OK) return READER_ERR;
 	
 	/* Initialisation de la HAL , init du SYSCLK etc ...*/
-	HAL_Init();
+	if(HAL_Init() != HAL_OK) return READER_ERR;
 	
 				/* Enable Power Control clock */
 	__HAL_RCC_PWR_CLK_ENABLE();
@@ -31,7 +31,7 @@ READER_Status READER_HAL_Init(void){
 	RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
 	RCC_OscInitStruct.PLL.PLLQ = 7;
 	
-	HAL_RCC_OscConfig(&RCC_OscInitStruct);
+	if(HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK) return READER_ERR;
 	
 	
 	RCC_ClkInitStruct.ClockType = (RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2);
@@ -40,8 +40,9 @@ READER_Status READER_HAL_Init(void){
 	RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV4;  
 	RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV2; 
 
-	HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_5);
+	if(HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_5) != HAL_OK) return READER_ERR;
 	
+	return READER_OK;
 }
 
 
