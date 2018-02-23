ddaee14cc34a9ece84884ff3e468304982957bbc
boris <boris.simunovic@etu.esisar.grenoble-inp.fr>
2018-02-23 15:11:54 +0100

    Ajout des fichiers READER_ATR.C et READER_ATR_H Definition des premiers types et constantes

---
+++ inc/reader_atr.h
@@ -0,0 +1,43 @@
+#define READER_ATR_MAX_HIST_BYTES 15;
+
+
+typedef enum READER_ATR_ClockStopIndicator READER_ATR_ClockStopIndicator:
+enum READER_ATR_ClockStopIndicator{
+	READER_ATR_CLOCKSTOP_NOTSUPPORTED    =   (uint32_t)(0x00000000),
+	READER_ATR_CLOCKSTOP_STATE_L         =   (uint32_t)(0x00000001),
+	READER_ATR_CLOCKSTOP_STATE_H         =   (uint32_t)(0x00000002),
+	READER_ATR_CLOCKSTOP_NOPREF          =   (uint32_t)(0x00000003)
+};
+
+
+
+typedef enum READER_ATR_ClassIndicator READER_ATR_ClassIndicator;
+READER_ATR_ClassIndicator{
+	READER_ATR_CLASS_A_ONLY              =   (uint32_t)(0x00000001),
+	READER_ATR_CLASS_B_ONLY              =   (uint32_t)(0x00000002),
+	READER_ATR_CLASS_C_ONLY              =   (uint32_t)(0x00000004),
+	READER_ATR_CLASS_AB_ONLY             =   (uint32_t)(0x00000003),
+	READER_ATR_CLASS_BC_ONLY             =   (uint32_t)(0x00000006),
+	READER_ATR_CLASS_ABC                 =   (uint32_t)(0x00000007)
+};
+
+
+typedef enum READER_ATR_UseOfSPU READER_ATR_UseOfSPU;
+enum READER_ATR_UseOfSPU{
+	READER_ATR_SPU_STANDARD              =   (uint32_t)(0x00000002),
+	READER_ATR_SPU_PROPRIETARY           =   (uint32_t)(0x00000001),
+	READER_ATR_SPU_NOTUSED               =   (uint32_t)(0x00000000)
+};
+
+
+typedef struct READER_ATR_Atr READER_ATR_Atr;
+struct READER_ATR_Atr{
+	uint32_t Fi;
+	uint32_t Di;
+	uint32_t fMax;
+	READER_ATR_ClockStopIndicator clockStopIndicator;
+	READER_ATR_ClassIndicator classIndicator;
+	READER_ATR_UseOfSPU useOfSPU;
+	uint32_t K;
+	uint8_t histBytes[READER_ATR_MAX_HIST_BYTES];
+};
