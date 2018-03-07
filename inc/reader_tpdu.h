594b4697e12207429c11e2dd1c59f37022383d3c
boris <boris.simunovic@etu.esisar.grenoble-inp.fr>
2018-03-07 14:26:49 +0100

    Remaniement des structures de donees. Separation du HEADER TP et du DATAFIELD TPDU Ajout des prototypes des fonctions d'envoi de TPDU

--- inc/reader_tpdu.h
+++ inc/reader_tpdu.h
@@ -11,18 +11,34 @@
 
 
 
-
-typedef struct READER_TPDU_Tpdu READER_TPDU_TpduCommand;
-struct READER_TPDU_TpduCommand{
+typedef struct READER_TPDU_Header READER_TPDU_Header;
+struct READER_TPDU_Header{
 	uint8_t CLA;
 	uint8_t INS;
 	uint8_t P1;
 	uint8_t P2;
 	uint8_t P3;
-	
-	uint8_t dataSize;
-	uint8_t dataField[READER_TPU_MAX_DATA];
 };
+
+
+typedef struct READER_TPDU_DataField READER_TPDU_DataField;
+struct READER_TPDU_DataField{
+	uint8_t size;
+	uint8_t data[READER_TPU_MAX_DATA];
+};
+
+
+typedef struct READER_TPDU_Tpdu READER_TPDU_Command;
+struct READER_TPDU_Command{
+	READER_TPDU_Header header;
+	READER_TPDU_DataField dataField;
+};
+
+
+
+READER_Status READER_TPDU_Send(READER_TPDU_Command *tpdu);
+READER_Status READER_TPDU_SendHeader(READER_TPDU_Header *tpduHeader);
+READER_Status READER_TPDU_SendDataOneshot(READER_TPDU_DataField *tpduDataField);
 
 
 
