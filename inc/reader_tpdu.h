a9ed5daa48df4f684864a143a18a141ff515c4b9
boris <boris.simunovic@etu.esisar.grenoble-inp.fr>
2018-03-07 14:16:17 +0100

    Ajout de la structure de donnees READER_TPDU_Tpdu

--- inc/reader_tpdu.h
+++ inc/reader_tpdu.h
@@ -7,6 +7,22 @@
 
 
 
+#define READER_TPU_MAX_DATA     0xFF;
+
+
+
+
+typedef struct READER_TPDU_Tpdu READER_TPDU_Tpdu;
+struct READER_TPDU_Tpdu{
+	uint8_t CLA;
+	uint8_t INS;
+	uint8_t P1;
+	uint8_t P2;
+	uint8_t P3;
+	
+	uint8_t dataSize;
+	uint8_t dataField[READER_TPU_MAX_DATA];
+};
 
 
 
