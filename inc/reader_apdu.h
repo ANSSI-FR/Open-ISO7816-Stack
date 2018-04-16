f3e5fbad738b0139ae7d03576268ee7427b3a80c
boris <boris.simunovic@etu.esisar.grenoble-inp.fr>
2018-04-16 15:42:14 +0200

    Définition des structures : 	READER_APDU_Command 	READER_APDU_Header 	READER_APDU_Body

--- inc/reader_apdu.h
+++ inc/reader_apdu.h
@@ -8,8 +8,28 @@
 
 
 
+typedef struct READER_APDU_Body READER_APDU_Body;
+struct READER_APDU_Body{
+	uint16_t Nc;
+	uint16_t Ne;
+	uint8_t *pDataField;
+};
 
 
+typedef struct READER_APDU_Header READER_APDU_Header;
+struct READER_APDU_Header{
+	uint8_t CLA;
+	uint8_t INS;
+	uint8_t P1;
+	uint8_t P2;
+};
+
+
+typedef struct READER_APDU_Command READER_APDU_Command;
+struct READER_APDU_Command{
+	READER_APDU_Header header;
+	READER_APDU_Body body;
+};
 
 
 
