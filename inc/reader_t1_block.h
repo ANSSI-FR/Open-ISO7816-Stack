a52a8927e23a9df17e0dbf66e60c972179a36b69
boris <boris.simunovic@etu.esisar.grenoble-inp.fr>
2018-07-09 19:03:54 +0200

    Dernier commit journée.

--- inc/reader_t1_block.h
+++ inc/reader_t1_block.h
@@ -1,9 +1,67 @@
-#define READER_T1_SBLOCK 0
+#include "reader.h"
 
 
-typedef enum READER_T1_BlocType READER_T1_BlocType;
-enum READER_T1_BlocType{
+#define READER_T1_BLOC_MAX_DATA_SIZE       254
+
+
+
+typedef enum READER_T1_RedundancyType READER_T1_RedundancyType;
+enum READER_T1_RedundancyType{
+	READER_T1_CRC                      = (uint32_t)(0x00000000),
+	READER_T1_LRC                      = (uint32_t)(0x00000001)
+};
+
+
+
+
+typedef enum READER_T1_BlockType READER_T1_BlockType;
+enum READER_T1_BlockType{
 	READER_T1_SBLOCK                   = (uint32_t)(0x00000000),
 	READER_T1_RBLOCK                   = (uint32_t)(0x00000001),
 	READER_T1_IBLOCK                   = (uint32_t)(0x00000002)
-}
+};
+
+
+typedef struct READER_T1_Block READER_T1_Block;
+struct READER_T1_Block{
+	uint8_t NAD;
+	uint8_t PCB;
+	uint8_t LEN;
+	uint8_t data[READER_T1_BLOC_MAX_DATA_SIZE];
+	READER_T1_RedundancyType RedundancyType;
+	uint8_t LRC;
+	uint16_t CRC;
+};
+
+
+READER_Status READER_T1_SetBlockSAD(READER_T1_Block *pBlock, uint8_t SAD);
+READER_Status READER_T1_SetBlockDAD(READER_T1_Block *pBlock, uint8_t DAD);
+READER_Status READER_T1_SetBlockNAD(READER_T1_Block *pBlock, uint8_t NAD);
+READER_Status READER_T1_SetBlockPCB(READER_T1_Block *pBlock, uint8_t PCB);
+READER_Status READER_T1_SetBlockType(READER_T1_Block *pBlock, READER_T1_BlockType type);
+READER_Status READER_T1_SetBlockLEN(READER_T1_Block *pBlock, uint8_t LEN);
+READER_Status READER_T1_SetBlockRedundancyType(READER_T1_Block *pBlock, READER_T1_RedundancyType type);
+READER_Status READER_T1_SetBlockLRC(READER_T1_Block *pBlock, uint8_t LRC);
+READER_Status READER_T1_SetBlockCRC(READER_T1_Block *pBlock, uint16_t CRC);
+READER_Status READER_T1_ComputeBlockLRC(READER_T1_Block *pBlock);
+READER_Status READER_T1_ComputeBlockCRC(READER_T1_Block *pBlock);
+READER_Status READER_T1_SetBlockData(READER_T1_Block *pBlock, uint8_t *data, uint8_t dataSize);
+
+
+uint8_t READER_T1_GetBlockSAD(const READER_T1_Block *pBlock);
+uint8_t READER_T1_GetBlockDAD(const READER_T1_Block *pBlock);
+uint8_t READER_T1_GetBlockNAD(const READER_T1_Block *pBlock);
+uint8_t READER_T1_GetBlockPCB(const READER_T1_Block *pBlock);
+READER_T1_BlockType READER_T1_GetBlockType(const READER_T1_Block *pBlock);
+uint8_t READER_T1_GetBlockLEN(const READER_T1_Block *pBlock);
+READER_T1_RedundancyType READER_T1_GetBlockRedundancyType(const READER_T1_Block *pBlock);
+uint8_t READER_T1_GetBlockLRC(const READER_T1_Block *pBlock);
+uint16_t READER_T1_GetBlockCRC(const READER_T1_Block *pBlock);
+uint8_t* READER_T1_GetBlockData(const READER_T1_Block *pBlock);
+
+
+
+READER_Status READER_T1_ForgeBlock(READER_T1_Block *pBlock);
+READER_Status READER_T1_SendBlock(READER_T1_Block *pBlock, uint32_t timeout);
+READER_Status READER_T1_RcvBlock(READER_T1_Block *pBlock, uint32_t timeout);
+
