806c31c9e3e14f25a6ff24c6f9302f75f3adf55f
boris <boris.simunovic@etu.esisar.grenoble-inp.fr>
2018-07-11 09:02:58 +0200

    Ajout du code de la fonction READER_T1_GetBlockNAD().

--- src/reader_t1_block.c
+++ src/reader_t1_block.c
@@ -177,7 +177,12 @@ uint8_t READER_T1_GetBlockDAD(const READER_T1_Block *pBlock){
 
 
 uint8_t READER_T1_GetBlockNAD(const READER_T1_Block *pBlock){
+	uint8_t *pCurrentNAD;
 	
+	
+	pCurrentNAD = pBlock->blockFrame + READER_T1_BLOCKFRAME_NAD_POSITION;
+	
+	return *pCurrentNAD;
 }
 
 
