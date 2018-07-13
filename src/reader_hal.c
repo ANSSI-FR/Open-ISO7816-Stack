f3407d52a75e29d66cb4ac0a00c9167460e64cf9
boris <boris.simunovic@etu.esisar.grenoble-inp.fr>
2018-07-13 16:43:10 +0200

    Ajout du code de la fonction READER_HAL_SetBWI().

--- src/reader_hal.c
+++ src/reader_hal.c
@@ -692,7 +692,26 @@ READER_Status READER_HAL_SetBWT(uint32_t BWT){
 
 
 READER_Status READER_HAL_SetBWI(uint32_t BWI){
+	READER_Status retVal;
+	uint32_t BWTEtu, BWTMili;
+	uint32_t currentFreq, cuurentF, currentD;
 	
+	currentFreq = READER_HAL_GetFreq();
+	currentF = READER_HAL_GetFi();
+	currentD = READER_HAL_GetDi();
+	
+	BWTEtu = READER_UTILS_ComputeBWTEtu(BWI, currentFreq);
+	BWTMili = READER_UTILS_ComputeBWTMili(BWTEtu, currentF, currentD, currentFreq);
+	
+	/* On modifie le BWT en consequence  */
+	retVal = READER_HAL_SetBWT(BWTMili);
+	if(retVal != READER_OK) return retVal;
+	
+	
+	globalCurrentSettings.BWI = BWI;
+	
+	
+	return READER_OK;
 }
 
 
