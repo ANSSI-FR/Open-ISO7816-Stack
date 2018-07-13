5cea66ee88784fbb22faaaaa7751bbc57181a45c
boris <boris.simunovic@etu.esisar.grenoble-inp.fr>
2018-07-13 15:23:18 +0200

    Ajout des constantes : READER_DEFAULT_BWI, READER_DEFAULT_BWT, READER_DEFAULT_BGT, READER_DEFAULT_IFSC, READER_DEFAULT_IFSD.

--- inc/reader.h
+++ inc/reader.h
@@ -9,7 +9,11 @@
 #define READER_DEFAULT_GT         (uint32_t)(12)
 #define READER_DEFAULT_WI         (uint32_t)(10)
 //#define READER_DEFAULT_WT_MILI    (uint32_t)((1000 * READER_DEFAULT_WI * 960 * READER_DEFAULT_FI) / (float)READER_DEFAULT_FREQ)      /* Voir ISO7816-3 section 10.2 */
-
+#define READER_DEFAULT_BGT        (uint32_t)(22)      /* 22 etu, Voir ISO7816-3 section 11.2 */
+#define READER_DEFAULT_BWI        (uint32_t)(4)       /* Voir ISO7816-3 section 11.4.3       */
+#define READER_DEFAULT_BWT        (uint32_t)(11 + 2*2*2*2 * 960 * ((float)READER_DEFAULT_FI / (float)READER_DEFAULT_FREQ))    /* Voir ISO7816-3 section 11.4.3       */
+#define READER_DEFAULT_IFSC       (uint32_t)(32)
+#define READER_DEFAULT_IFSD       (uint32_t)(32)
 
 
 
