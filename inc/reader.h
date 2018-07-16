6aab3a560ce82753f1c463b6d04bcec73b882a43
boris <boris.simunovic@etu.esisar.grenoble-inp.fr>
2018-07-16 10:58:07 +0200

    Creation de la constante READER_DEFAULT_REDUNDANCY_TYPE.

--- inc/reader.h
+++ inc/reader.h
@@ -2,6 +2,8 @@
 #define __READER_H__
 
 
+#include "reader_t1_block.h"
+
 
 #define READER_DEFAULT_FI         (uint32_t)(372)
 #define READER_DEFAULT_DI         (uint32_t)(1)
@@ -14,6 +16,7 @@
 #define READER_DEFAULT_BWT        (uint32_t)(11 + 2*2*2*2 * 960 * ((float)READER_DEFAULT_FI / (float)READER_DEFAULT_FREQ))    /* Voir ISO7816-3 section 11.4.3       */
 #define READER_DEFAULT_IFSC       (uint32_t)(32)
 #define READER_DEFAULT_IFSD       (uint32_t)(32)
+#define READER_DEFAULT_REDUNDANCY_TYPE READER_T1_LRC
 
 
 
