af26713e2cac06be368ab461747e270537ecd759
boris <boris.simunovic@etu.esisar.grenoble-inp.fr>
2018-07-17 18:41:50 +0200

    Ajout du code de la fonction READER_T1_ForgeRBlock().

--- src/reader_t1_rblock.c
+++ src/reader_t1_rblock.c
@@ -101,3 +101,21 @@ READER_T1_SeqNumber READER_T1_GetExpectedBlockSeqNumber(READER_T1_Block *pBlock)
 	}
 }
 
+
+
+READER_Status READER_T1_ForgeRBlock(READER_T1_Block *pBlock, READER_T1_ACKType ack, READER_T1_SeqNumber expctdBlockSeq){
+	READER_T1_RedundancyType rType;
+	READER_Status retVal;
+	
+	rType = READER_HAL_GetRedunancyType();
+	retVal = READER_T1_ForgeBlock(pBlock, rType);
+	if(retVal != READER_OK) return retVal;
+	
+	retVal = READER_T1_SetBlockACKType(pBlock, ack);
+	if(retVal != READER_OK) return retVal;
+	
+	retVal = READER_T1_SetExpectedBlockSeqNumber(pBlock, expctdBlockSeq);
+	if(retVal != READER_OK) return retVal;
+	
+	return READER_OK;
+}
