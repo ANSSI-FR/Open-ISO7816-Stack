4d847b7e501c6776e6b7bb84cf61f6128bb32e94
boris <boris.simunovic@etu.esisar.grenoble-inp.fr>
2018-07-17 18:07:33 +0200

    Ajout du code des fonctions READER_T1_SetExpectedBlockSeqNumber() et READER_T1_GetExpectedBlockSeqNumber().

--- src/reader_t1_rblock.c
+++ src/reader_t1_rblock.c
@@ -37,3 +37,46 @@ READER_Status READER_T1_SetBlockACKType(READER_T1_Block *pBlock, READER_T1_ACKTy
 }
 
 
+
+READER_Status READER_T1_SetExpectedBlockSeqNumber(READER_T1_Block *pBlock, READER_T1_SeqNumber seq){
+	/* Voir ISO7816-3 section 11.3.2.2 */
+	READER_Status retVal;
+	uint8_t currentPCB, newPCB;
+	
+	currentPCB = READER_T1_GetBlockPCB(pBlock);
+	
+	if(seq == READER_T1_SEQNUM_ZERO){
+		newPCB = currentPCB & 0xBF ;
+	}
+	else if(seq == READER_T1_SEQNUM_ONE){
+		newPCB = currentPCB | ~0xBF ;
+	}
+	else{
+		return READER_ERR;
+	}
+	
+	
+	retVal = READER_T1_SetBlockPCB(pBlock, newPCB);
+	if(retVal != READER_OK) return retVal;
+	
+	return READER_OK;
+}
+
+
+READER_T1_SeqNumber READER_T1_GetExpectedBlockSeqNumber(READER_T1_Block *pBlock){
+	/* Voir ISO7816-3 section 11.3.2.2 */
+	READER_Status retVal;
+	uint8_t currentPCB, newPCB;
+	
+	
+	
+	currentPCB = READER_T1_GetBlockPCB(pBlock);
+	
+	if((currentPCB & ~0xBF) == 0x00){
+		return READER_T1_SEQNUM_ZERO;
+	}
+	else{
+		return READER_T1_SEQNUM_ONE;
+	}
+}
+
