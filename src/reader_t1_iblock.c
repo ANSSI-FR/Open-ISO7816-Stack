9d8567caa2a4ec8fd3603a84211dbfaa94c79f89
boris <boris.simunovic@etu.esisar.grenoble-inp.fr>
2018-07-17 17:31:47 +0200

    Ajout du code de la fonction READER_T1_GetBlockMBit().

--- src/reader_t1_iblock.c
+++ src/reader_t1_iblock.c
@@ -76,7 +76,20 @@ READER_T1_SeqNumber READER_T1_GetBlockSeqNumber(READER_T1_Block *pBlock){
 
 
 READER_T1_MBit READER_T1_GetBlockMBit(READER_T1_Block *pBlock){
+	/* Voir ISO7816-3 section 11.3.2.2 */
+	READER_Status retVal;
+	uint8_t currentPCB, newPCB;
 	
+	
+	
+	currentPCB = READER_T1_GetBlockPCB(pBlock);
+	
+	if((currentPCB & ~0xDF) == 0x00){
+		return READER_T1_MBIT_ZERO;
+	}
+	else{
+		return READER_T1_MBIT_ONE;
+	}
 }
 
 
