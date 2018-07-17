139a15595cfd68057890e11cbeffc4f036c5d559
boris <boris.simunovic@etu.esisar.grenoble-inp.fr>
2018-07-17 16:04:42 +0200

    Ajout des prototype des fonctions concernant les R-BLOCK. et structures des données.

--- inc/reader_t1_rblock.h
+++ inc/reader_t1_rblock.h
@@ -3,4 +3,26 @@
 
 
 
+
+typedef enum READER_T1_ACKType READER_T1_ACKType;
+enum READER_T1_ACKType{
+	READER_T1_ACKTYPE_ACK               =     (uint32_t)(0x00000000),
+	READER_T1_ACKTYPE_NACK              =     (uint32_t)(0x00000001),
+	READER_T1_ACKTYPE_NACK_CRCLRC       =     (uint32_t)(0x00000002)
+};
+
+
+
+
+READER_Status READER_T1_SetBlockACK(READER_T1_Block *pBlock, READER_T1_ACKType ack);
+READER_T1_ACKType READER_T1_GetBlockACK(READER_T1_Block *pBlock);
+
+READER_Status READER_T1_ForgeRBlock(READER_T1_Block *pBlock, READER_T1_ACKType ack);
+
+READER_Status READER_T1_SendBlockACK(void);
+READER_Status READER_T1_SendBlockNACK(void);
+READER_Status READER_T1_SendBlockNACK_CRCLRC(void);
+
+
+
 #endif
