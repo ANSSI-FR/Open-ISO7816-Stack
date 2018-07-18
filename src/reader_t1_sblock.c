a6ca63aabe868be72fe7ec55f97390a67f35de02
boris <boris.simunovic@etu.esisar.grenoble-inp.fr>
2018-07-18 11:20:00 +0200

    Premiere ebauche code de la fonction READER_T1_ForgeSBlock().

--- src/reader_t1_sblock.c
+++ src/reader_t1_sblock.c
@@ -1,3 +1,24 @@
 #include "reader_t1_sblock.h"
 #include "reader_t1_block.h"
 #include "reader_hal.h"
+
+
+
+
+
+
+
+
+READER_Status READER_T1_ForgeSBlock(READER_T1_Block *pBlock, READER_T1_SBlockType type){
+	READER_T1_RedundancyType rType;
+	READER_Status retVal;
+	
+	
+	rType = READER_HAL_GetRedunancyType();
+	retVal = READER_T1_ForgeBlock(pBlock, rType);
+	if(retVal != READER_OK) return retVal;
+	
+	retVal = READER_T1_SetBlock
+	
+	return READER_OK;
+}
