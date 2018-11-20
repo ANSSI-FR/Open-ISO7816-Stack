15f15cd2740c17ba25be115425143e9ffbca6cd3
boris <boris.simunovic@etu.esisar.grenoble-inp.fr>
2018-11-20 14:22:04 +0100

    Correction d'erreur dans le ENUM BufferStatus.

--- inc/reader_t1_buffer_handler.h
+++ inc/reader_t1_buffer_handler.h
@@ -46,8 +46,8 @@ struct READER_T1_BlockBuffer{
 typedef enum READER_T1_BufferStatus READER_T1_BufferStatus;
 enum READER_T1_BufferStatus{
 	READER_T1_BUFFER_EMPTY             =       (uint32_t)(0x00000000),
-	READER_T1_SBLOCK_NOTEMPTY          =       (uint32_t)(0x00000001),
-	READER_T1_SBLOCK_FULL              =       (uint32_t)(0x00000002)
+	READER_T1_BUFFER_NOTEMPTY          =       (uint32_t)(0x00000001),
+	READER_T1_BUFFER_FULL              =       (uint32_t)(0x00000002)
 };
 
 
