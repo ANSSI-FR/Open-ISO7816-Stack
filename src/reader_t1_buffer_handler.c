71c2b1458f5fea0591daa2c7ddd106c0e8802d04
boris <boris.simunovic@etu.esisar.grenoble-inp.fr>
2019-04-02 16:43:31 +0200

    Modification de la fonction READER_T1_BUFFER_Enqueue() pour désormais utiliser la fonction READER_T1_BUFFER_IncLength().

--- src/reader_t1_buffer_handler.c
+++ src/reader_t1_buffer_handler.c
@@ -161,7 +161,8 @@ READER_Status READER_T1_BUFFER_Enqueue(READER_T1_ContextHandler *pContext, READE
 	pBlockBuffer->indexTop = newTopIndex;
 	
 	/* On mets a jour la length */
-	pBlockBuffer->length += 1;
+	retVal = READER_T1_BUFFER_IncLength(pContext);
+	if(retVal != READER_OK) return retVal;
 	
 	/* On mets a jour le nombre de I-Blocks dans le Buffer d'envoi ...  */
 	bType = READER_T1_GetBlockType(pBlock);
