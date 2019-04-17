0f606ea9b25adfeda27cecf731d05d70234544fa
boris <boris.simunovic@etu.esisar.grenoble-inp.fr>
2019-04-17 11:32:58 +0200

    Ajout du test test_READER_TPDU_IsACK_shouldWork().

--- tests/reader_tests_tpdu.h
+++ tests/reader_tests_tpdu.h
@@ -16,6 +16,7 @@ void test_READER_TPDU_Forge_shouldCopyData(void);
 void test_READER_TPDU_SendHeader_shouldSendRightFrame(void);
 void test_READER_TPDU_SendDataOneshot_shouldSendRightFrame(void);
 void test_READER_TPDU_SendDataOneshot_shouldVerifyDataSize(void);
+void test_READER_TPDU_IsACK_shouldWork(void);
 
 
 
