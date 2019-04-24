2f0a33f2f89204dd43ea808f1b204c020acc5444
boris <boris.simunovic@etu.esisar.grenoble-inp.fr>
2019-04-24 11:20:23 +0200

    Ajout de la constante READER_LESS_DATA_THAN_EXPECTED à la structure READER_Status.

--- inc/reader.h
+++ inc/reader.h
@@ -11,23 +11,24 @@
 
 typedef enum READER_Status READER_Status;
 enum READER_Status{
-	READER_OK                   = (uint32_t)(0x00000001),
-	READER_NO                   = (uint32_t)(0x00000000),
-	READER_ERR                  = (uint32_t)(0x00000002),
-	READER_TIMEOUT              = (uint32_t)(0x00000003),
-	READER_TIMEOUT_GOT_ONLY_SW  = (uint32_t)(0X00000004),
-	READER_BUSY                 = (uint32_t)(0x00000005),
-	READER_TOO_MUCH_TRIES       = (uint32_t)(0x00000006),
-	READER_EMPTY                = (uint32_t)(0x00000007),
-	READER_FULL                 = (uint32_t)(0x00000008),
-	READER_BAD_ARG              = (uint32_t)(0x00000009),
-	READER_TOO_LONG             = (uint32_t)(0x0000000A),
-	READER_DOESNT_EXIST         = (uint32_t)(0x0000000B),
-	READER_INTEGRITY            = (uint32_t)(0x0000000C),
-	READER_BLOCK_TIMEOUT        = (uint32_t)(0x0000000D),
-	READER_BAD_VALUE            = (uint32_t)(0x0000000E),
-	READER_INVALID_BLOCK        = (uint32_t)(0x0000000F),
-	READER_OVERFLOW             = (uint32_t)(0x00000010)
+	READER_OK                       = (uint32_t)(0x00000001),
+	READER_NO                       = (uint32_t)(0x00000000),
+	READER_ERR                      = (uint32_t)(0x00000002),
+	READER_TIMEOUT                  = (uint32_t)(0x00000003),
+	READER_TIMEOUT_GOT_ONLY_SW      = (uint32_t)(0X00000004),
+	READER_BUSY                     = (uint32_t)(0x00000005),
+	READER_TOO_MUCH_TRIES           = (uint32_t)(0x00000006),
+	READER_EMPTY                    = (uint32_t)(0x00000007),
+	READER_FULL                     = (uint32_t)(0x00000008),
+	READER_BAD_ARG                  = (uint32_t)(0x00000009),
+	READER_TOO_LONG                 = (uint32_t)(0x0000000A),
+	READER_DOESNT_EXIST             = (uint32_t)(0x0000000B),
+	READER_INTEGRITY                = (uint32_t)(0x0000000C),
+	READER_BLOCK_TIMEOUT            = (uint32_t)(0x0000000D),
+	READER_BAD_VALUE                = (uint32_t)(0x0000000E),
+	READER_INVALID_BLOCK            = (uint32_t)(0x0000000F),
+	READER_OVERFLOW                 = (uint32_t)(0x00000010),
+	READER_LESS_DATA_THAN_EXPECTED  = (uint32_t)(0x00000011)
 };
 
 
