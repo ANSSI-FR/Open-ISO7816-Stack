b41d1c4519acdacb976863f656ac201ebc82032f
boris <boris.simunovic@etu.esisar.grenoble-inp.fr>
2019-07-02 16:14:39 +0200

    Ajout des fichiers reader_tests_block.c et reader_tests_block.h

---
+++ tests/reader_tests_block.c
@@ -0,0 +1,18 @@
+#include "unity.h"
+#include "mock_reader_hal.h"
+#include "mock_reader_hal_comm_settings.h"
+#include "mock_reader_periph.h"
+
+#include "reader_block.h"
+
+#include "reader_tests_block.h"
+
+#include <time.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+void test_READER_BLOCK_all(void){
+	
+}
---
+++ tests/reader_tests_block.h
@@ -0,0 +1,16 @@
+#ifndef __READER_TESTS_BLOCK_H__
+#define __READER_TESTS_BLOCK_H__
+
+
+
+#include "reader_block.h"
+
+
+
+
+
+void test_READER_BLOCK_all(void);
+
+
+
+#endif
