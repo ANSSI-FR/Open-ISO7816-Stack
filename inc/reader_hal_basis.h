3943381690a9416d7895ed4bb9f09ee0a8d656ae
boris <boris.simunovic@etu.esisar.grenoble-inp.fr>
2019-07-04 16:07:44 +0200

    Ajout des fichiers reader_hal_basis.h et reader_hal_basis.c.

---
+++ inc/reader_hal_basis.h
@@ -0,0 +1,15 @@
+#ifndef __READER_HAL_BASIS_H__
+#define __READER_HAL_BASIS_H__
+
+
+#include "reader.h"
+#include "reader_periph.h"
+#include "reader_utils.h"
+#include "reader_hal_comm_settings.h"
+#include "reader_atr.h"
+#include <stdint.h>
+
+
+
+
+#endif
---
+++ src/reader_hal_basis.c
@@ -0,0 +1,2 @@
+#include "reader_hal_basis.h"
+#include "stm32f4xx_hal.h"
