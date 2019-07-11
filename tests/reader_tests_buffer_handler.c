d4d851c5f67ff37af76ca2bf37b84c26096e09c5
boris <boris.simunovic@etu.esisar.grenoble-inp.fr>
2019-07-11 14:55:44 +0200

    Ajout des fichiers reader_tests_buffer_handler.c reader_tests_buffer_handler.h.

---
+++ tests/reader_tests_buffer_handler.c
@@ -0,0 +1,24 @@
+#include "unity.h"
+#include "mock_reader_hal_basis.h"
+#include "mock_reader_hal_comm_settings.h"
+#include "mock_reader_periph.h"
+
+#include "reader_t1_buffer_handler.h"
+
+#include "reader_tests_buffer_handler.h"
+#include "reader_tests_test.h"
+
+#include <time.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+
+
+
+
+void test_READER_BUFFER_all(void){
+	
+	
+}
---
+++ tests/reader_tests_buffer_handler.h
@@ -0,0 +1,19 @@
+#ifndef __READER_TESTS_BUFFER_HANDLER_H__
+#define __READER_TESTS_BUFFER_HANDLER_H__
+
+
+#include "reader_t1_buffer_handler.h"
+
+
+
+
+
+void test_READER_BUFFER_all(void);
+
+
+
+
+
+
+
+#endif
